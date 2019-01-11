713ff7c1cceb7b094c2c028a8a8f68961e0a0cf2
ZKC952 <1657520995@qq.com>
2019-01-11 14:01:43 +0800

    两种存储格式

--- Jobs/run.py
+++ Jobs/run.py
@@ -8,8 +8,8 @@ settings = get_project_settings()
 # 重写配置
 user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
 settings.set('USER_AGENT', user_agent, priority='cmdline')  # UserAgent
-settings.set('FEED_FORMAT', 'json', priority='cmdline')  # 结果格式
-settings.set('FEED_URI', 'result.json', priority='cmdline')  # 结果地址
+# settings.set('FEED_FORMAT', 'json', priority='cmdline')  # 结果格式
+# settings.set('FEED_URI', 'result.json', priority='cmdline')  # 结果地址
 # 爬虫进程
 process = CrawlerProcess(settings=settings)
 # 爬虫加载器
@@ -20,11 +20,15 @@ for spider_name in loader.list():
     print(spider_name)
     # 得到爬虫类
     SpiderClass = loader.load(spider_name)
-    # 结果存储地址
-    feed_uri = 'result-{}.json'.format(spider_name)
-    # 重写当前爬虫的数据存储地址
-    settings.set('FEED_URI', feed_uri, priority='cmdline')
+    # # 结果存储地址
+    # feed_uri = 'result-{}.json'.format(spider_name)
+    # # 重写当前爬虫的数据存储地址
+    # settings.set('FEED_URI', feed_uri, priority='cmdline')
+    settings.set('MONGO_DATABASE', spider_name, priority='cmdline')
     # 添加爬取任务
     process.crawl(SpiderClass)
 # 启动爬虫进程
 process.start()
+
+# 存储格式：
+#       11,12,26行取消注释,27注释  --- 保存json文件  反之  --- 保存到mongoDB
